1ed32b80cd50c4a7a5ea20e4b8a43dedb3bfe00f
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2022-08-15 00:36:03 +0300

    14/08/2022

--- DataAccess/Concrete/EntityFramework/EfOrderDal.cs
+++ DataAccess/Concrete/EntityFramework/EfOrderDal.cs
@@ -1,18 +1,14 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    //NuGet: kodların ortak koyulduğu ve yönetildiği ortam.
-    public class EfProductDal :EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    public class EfOrderDal : EfEntityRepositoryBase<Order, NorthwindContext>, IOrderDal
     {
-       
     }
 }
